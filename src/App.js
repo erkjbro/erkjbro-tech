872c295d37d837ea89361e600a9f98b6cd05dd9a
Erik J Brown <erkjbro@gmail.com>
2020-11-19 11:08:12 -0600

    code cleanup for route prep

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 
+const routes = (
+  <>
+    <h1>Erik J Brown Tech LLC</h1>
+    <p className="Alert">
+      This website currently under development.
+      <br />
+      Visit <a href="https://erkjbro.github.io/">erkjbro.github.io</a> for
+      my old portfolio site.
+    </p>
+  </>
+);
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Erik J Brown Tech LLC</h1>
-      <p className="Alert">
-        This website currently under development.
-        <br />
-        Visit <a href="https://erkjbro.github.io/">erkjbro.github.io</a> for
-        my old portfolio site.
-      </p>
+      <main>
+        {routes}
+      </main>
     </div>
   );
 };
