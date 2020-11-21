5a34bffe9a4b6322b2141d7c6e4e70475c768ccc
Erik J Brown <erkjbro@gmail.com>
2020-11-20 18:03:58 -0600

    starting homepage setup & styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,16 @@
+import './Home.css';
+
 const Home = () => (
-  <>
+  <div className="Home">
     <h1>Erik J Brown Tech LLC</h1>
-    <p className="Alert">
+
+    <p>
       This website currently under development.
       <br />
       Visit <a href="https://erkjbro.github.io/">erkjbro.github.io</a> for
       my old portfolio site.
     </p>
-  </>
+  </div>
 );
 
 export default Home;
