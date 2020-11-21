f6c99113aef58ff4131f712165085e495544712d
Erik J Brown <erkjbro@gmail.com>
2020-11-21 13:34:19 -0600

    creating reusable navlinks

---
+++ src/components/Navigation/NavLinks/NavLinks.js
@@ -0,0 +1,13 @@
+import { NavLink } from 'react-router-dom';
+
+import './NavLinks.css';
+
+const NavLinks = () => (
+  <ul className="nav-links">
+    <li>
+      <NavLink to="/" exact>HOME</NavLink>
+    </li>
+  </ul>
+);
+
+return NavLinks;
