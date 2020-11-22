6b6f1893dbfcc6994dfb29742360f111bc9e3416
Erik J Brown <erkjbro@gmail.com>
2020-11-22 16:41:51 -0600

    info on tech & tools added

--- src/pages/Tech/Tech.js
+++ src/pages/Tech/Tech.js
@@ -2,8 +2,33 @@ import './Tech.scss';
 
 const Tech = () => {
   return (
-    <div>
-      Tech
+    <div className="Tech">
+      <h1>Tech & Tools</h1>
+
+      <h2>Development</h2>
+      <ul>
+        <li>React</li>
+        <li>Node & Express</li>
+        <li>MongoDB</li>
+      </ul>
+
+      <h2>DevOps</h2>
+      <ul>
+        <li>Ubuntu</li>
+        <li>Docker</li>
+        <li>Firebase</li>
+        <li>MongoDB Atlas</li>
+        <li>GitHub</li>
+      </ul>
+
+      <h2>Tools</h2>
+      <ul>
+        <li>VS Code</li>
+        <li>MongoDB Compass</li>
+        <li>Postman</li>
+        <li>Jira</li>
+        <li>Confluence</li>
+      </ul>
     </div>
   );
 };
