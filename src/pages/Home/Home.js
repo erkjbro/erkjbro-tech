9d833f1a82d6c68e7da1a29476be54d9d38fb80f
Erik J Brown <erkjbro@gmail.com>
2020-12-19 15:31:05 -0600

    updating links & borders

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,23 +6,22 @@ const PARAGRAPHS = [
   {
     id: 1,
     value: `
-      Hello! I'm a full-stack web app developer that's passionate about the MERN stack.
+      Hey there! My name is Erik and I am a full-stack web developer that is passionate about the MERN stack.
     `
   },
   {
     id: 2,
     value: `
-      Lately I have been using Digital Ocean to host my droplets (ec2 instances), Google Firebase
-      for web client hosting, MongoDB Atlas for a cloud DBaaS, GitHub for a code repo, and then Namecheap
-      for domains. The droplet simply uses Nginx and PM2 for running the Node app.
+      Lately I have been using Digital Ocean to host my VMs, Google Firebase
+      for web client hosting, MongoDB Atlas for a cloud DBaaS, GitHub for a code repo, and just Namecheap
+      for domains. The VM simply uses Nginx and PM2 for running the Node apps.
     `
   },
   {
     id: 3,
     value: `
-      I haven't decided between AWS or DO for an S3 solution to host images. I also have been working with a
-      Docker Swarm of three droplets on DO, but I'd like to improve my Docker understanding further before
-      containerizing my apps.
+      I have yet to decided between AWS or DO for an S3 solution for images. I also have been working
+      with Docker Swarm nodes hosted on DO, but I need more Docker experience before changing my workflow.
     `
   },
   {
@@ -35,15 +34,15 @@ const PARAGRAPHS = [
   {
     id: 5,
     value: `
-      For project management I have been using Jira with kanban boards and also trying Confluence for project
-      documentation. I haven't settled on a time tracking app yet, and just use a pen and a time tracking journal.
+      For projects I have been using Jira with kanban boards, Confluence for project documentation,
+      and Lucidchart for charts. I haven't settled on a time tracking app yet.
     `
   },
   {
     id: 6,
     value: `
       I have a life outside of coding as well. Some of the things
-      I spend time on include mountain biking, bowling, video games, RC cars, and PCs.
+      I spend time on include mountain biking, video games, a little weight lifting, and custom PCs.
     `
   }
 ];
@@ -52,7 +51,7 @@ const PARAGRAPHS = [
 const Home = () => (
   <>
     <div className="Header">
-      <img src={ejbOrangeLogo} alt="Erik J Brown" />
+      <img src={ejbOrangeLogo} alt="ejb logo" />
     </div>
     <div className="Home">
       <h1>Erik J Brown Tech LLC</h1>
@@ -61,9 +60,11 @@ const Home = () => (
         {PARAGRAPHS.map(p => <p key={p.id}>{p.value}</p>)}
 
         <p>
-          If you're interested in contacting me about freelance opportunities or wish to network,
-          you can find me on LinkedIn, Upwork, GitHub, or email me
-          at <a href="mailto:erkjbro@erikjbrown.tech">erkjbro@erikjbrown.tech</a>
+          If you're interested in contacting me about freelance opportunities or wish to network, you can find me
+          on <a href="https://https://www.linkedin.com/in/erkjbro/">LinkedIn</a>
+          , <a href="https://www.upwork.com/freelancers/~01820d75b9551f9eea">Upwork</a>
+          , <a href="https://github.com/erkjbro">GitHub</a>
+          , or email me at <a href="mailto:erkjbro@erikjbrown.tech">erkjbro@erikjbrown.tech</a>
         </p>
       </div>
     </div>
