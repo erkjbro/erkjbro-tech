1719628326d00fb677daf828c15885b9445f5f18
Erik J Brown <erkjbro@gmail.com>
2020-12-19 13:49:47 -0600

    restructuring page data

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,54 @@
 import ejbOrangeLogo from '../../assets/images/ejb-orange-logo-v1.png';
 import './Home.scss';
 
+const PARAGRAPHS = [
+  {
+    id: 1,
+    value: `
+      I'm a full-stack web app developer who's passionate about the MERN stack.
+      This involves MongoDB, Express.js, React, and Node.js.
+    `
+  },
+  {
+    id: 2,
+    value: `
+      Lately I have been using Digital Ocean to host my droplets (ec2 instances), Google Firebase
+      for web client hosting, MongoDB Atlas for a cloud DBaaS, GitHub for a code repo, and then Namecheap
+      for domains. The droplet simply uses Nginx and PM2 for running the Node app.
+    `
+  },
+  {
+    id: 3,
+    value: `
+      I haven't decided between AWS or DO for an S3 solution to host images. I also have been working with a
+      Docker Swarm of three droplets on DO, but I'd like to improve my Docker understanding further before
+      containerizing my apps.
+    `
+  },
+  {
+    id: 4,
+    value: `
+      My primary tools are Visual Studio Code, MongoDB Compass, Postman, and Docker Desktop.
+      I like to use WSL2 w/ Ubuntu on Windows Terminal for my shell.
+    `
+  },
+  {
+    id: 5,
+    value: `
+      For project management I have been using Jira with kanban boards and also trying Confluence for project
+      documentation. I haven't settled on a time tracking app yet, and just use a pen and a time tracking journal.
+    `
+  },
+  {
+    id: 6,
+    value: `
+      I have a life outside of coding as well. Some of the things
+      I spend time on include mountain biking, bowling, video games, RC cars, and PCs.
+    `
+  },
+];
+
+
 const Home = () => (
   <>
     <div className="Header">
@@ -10,37 +58,7 @@ const Home = () => (
     <div className="Home">
       <h1>Erik J Brown Tech LLC</h1>
 
-      <p>
-        I'm a full-stack web app developer who's passionate about the <b>MERN</b> stack. This involves
-        MongoDB, Express.js, React, and Node.js.
-      </p>
-
-      <p>
-        Lately I have been using Digital Ocean to host my droplets (ec2 instances), Google Firebase
-        for web client hosting, MongoDB Atlas for a cloud DBaaS, GitHub for a code repo, and then Namecheap
-        for domains. The droplet simply uses Nginx and PM2 for running the Node app.
-      </p>
-
-      <p>
-        I haven't decided between AWS or DO for an S3 solution to host images. I also have been working with a
-        Docker Swarm of three droplets on DO, but I'd like to improve my Docker understanding further before
-        containerizing my apps.
-      </p>
-
-      <p>
-        My primary tools are <b>Visual Studio Code</b>, MongoDB Compass, Postman, & Docker Desktop.
-        I like to use WSL2 w/ Ubuntu on Windows Terminal for my shell.
-      </p>
-
-      <p>
-        For project management I have been using Jira with kanban boards and also trying Confluence for project documentation.
-        I haven't settled on a time tracking app yet, and just use a pen and a time tracking journal.
-      </p>
-
-      <p>
-        I have a life outside of coding as well. Some of the things
-        I spend time on include mountain biking, bowling, video games, RC cars, & PCs.
-      </p>
+      {PARAGRAPHS.map(p => <p key={p.id}>{p.value}</p>)}
 
       <p>
         If you're interested in contacting me about freelance opportunities or wish to network,
