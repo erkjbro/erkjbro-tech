e1a47118f752a80a694cf8d5a98161426fa5a725
Erik J Brown <erkjbro@gmail.com>
2021-04-15 10:24:15 -0500

    updating tech info

--- src/pages/Tech/Tech.js
+++ src/pages/Tech/Tech.js
@@ -3,31 +3,47 @@ import './Tech.scss';
 const Tech = () => {
   return (
     <div className="Tech">
-      <h1>Tech & Tools</h1>
+      <h1>Tech Stack &amp; Tools</h1>
 
       <h2>Development</h2>
       <ul>
         <li>React</li>
-        <li>Node & Express</li>
+        <li>Node &amp; Express</li>
         <li>MongoDB</li>
+      </ul>
+
+      <h2>AWS &amp; Cloud</h2>
+      <ul>
+        <li>Elastic Beanstalk (cli)</li>
+        <li>Amplify</li>
+        <li>MongoDB Atlas</li>
       </ul>
 
       <h2>DevOps</h2>
       <ul>
         <li>Ubuntu</li>
         <li>Docker</li>
-        <li>Firebase</li>
-        <li>MongoDB Atlas</li>
         <li>GitHub</li>
+      </ul>
+
+      <h2>Languages</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>CSS / SCSS</li>
+        <li>HTML</li>
+        <li>In the past: C, C++, C#, Java, Matlab, Assembly</li>
       </ul>
 
       <h2>Tools</h2>
       <ul>
         <li>VS Code</li>
         <li>MongoDB Compass</li>
+        <li>Sqlectron</li>
         <li>Postman</li>
+        <li>Lucidchart</li>
         <li>Jira</li>
         <li>Confluence</li>
+        <li>Everhour</li>
       </ul>
     </div>
   );
