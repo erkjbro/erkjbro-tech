6b5e2e63a2c42e0b7e50cb9f2819d4bc9d094c16
Erik J Brown <erkjbro@erikjbrown.tech>
2022-04-24 14:08:12 -0500

    Changing navbar colors

--- src/shared/uiKit/layout/Sitewide.tsx
+++ src/shared/uiKit/layout/Sitewide.tsx
@@ -1,15 +1,18 @@
-import { Container, type ContainerProps, type Breakpoint } from '@mui/material'
+import React from 'react';
+import { Container, type ContainerProps, type Breakpoint } from '@mui/material';
 
-import Logo from 'shared/assets/images/ejb-black-logo-v2.png'
+import Logo from 'shared/assets/images/ejb-black-logo-v2.png';
 
-const Sitewide: React.FC<ContainerProps> = ({children, ...rest}) => (
+const Sitewide: React.FC<ContainerProps> = ({ children, ...rest }) => (
   <Container
     disableGutters
     maxWidth={false}
     {...rest}
   >
-    <nav style={{ backgroundColor: '#a0e03a', display: 'flex', justifyContent: 'center', padding: '10px 0' }}>
-      <img src={Logo} alt="logo" width="50px"/>
+    <nav style={{ backgroundColor: '#11114a', display: 'flex', justifyContent: 'center', padding: '10px 0' }}>
+      <span style={{ backgroundColor: 'whitesmoke', padding: '10px', border: '3px solid darkorange', borderRadius: '50%' }}>
+        <img src={Logo} alt="logo" width="50px" />
+      </span>
     </nav>
     {children}
   </Container>
