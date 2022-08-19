35c11e585975c5ebea040c5f96f761314c4bfa35
Erik J Brown <erkjbro@erikjbrown.tech>
2022-08-19 17:48:05 -0500

    Update on backend skills

--- src/portfolio/pages/Portfolio.tsx
+++ src/portfolio/pages/Portfolio.tsx
@@ -46,19 +46,15 @@ const Portfolio = () => (
 
       <h3>Backend Tech</h3>
       <p>
-        I've built some monolithic nodejs apps as REST apis, but lately I'm working with Python.
-        The Python is focused on an ETL serverless step function that's architected with SAM.
-        App handlers pull information from a legacy db, which in turn gets processed and loaded
-        into an SQS Queue that is being watched by other handlers. DL Queues are configured
-        in case of exceptions. I also configured a serverless layer to hold some common
-        utils that are necessary for consumption within all of the app's lambdas. To enforce
-        code quality, I have a bitbucket pipeline on PRs that checks for passing results of
-        pytest, pylint, and flake8.
+        I've built some monolithic Node.js apps as REST APIs, but lately I'm working with Python for a
+        SAM-based ETL that takes care of data migration. It extracts data ids from the old platform
+        and loads it into an SQS queue. From there, concurrent lambdas pick up the messages to query data
+        by these ids and ETL that data into the new system.
       </p>
 
       <h3>Contact Info</h3>
       <ul>
-        <li><a href="mailto:erkjbro@erikjbrown.tech">erkjbro@erikjbrown.tech</a></li>
+        <li><a href="mailto:erkjbro@erikjbrown.tech">E-Mail</a></li>
         <li><a href="https://www.linkedin.com/in/erkjbro/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
         <li><a href="https://github.com/erkjbro" target="_blank" rel="noopener noreferrer">GitHub</a></li>
         <li><a href="https://twitter.com/erkjbro" target="_blank" rel="noopener noreferrer">Twitter</a></li>
