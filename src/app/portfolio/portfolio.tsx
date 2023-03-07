c3c08589034b22d150096c9f80f6c1a4bd96de49
Erik J Brown <erkjbro@erikjbrown.tech>
2023-03-06 21:02:42 -0600

    Displaying links dynamically based upon provided data

--- src/app/portfolio/portfolio.tsx
+++ src/app/portfolio/portfolio.tsx
@@ -1,11 +1,20 @@
-import { FC } from 'react';
-import { Hr, Main, PortfolioContainer, StyledList } from './portfolio-styled';
+import { FC, ReactNode } from "react";
+import { Hr, Main, PortfolioContainer, StyledList } from "./portfolio-styled";
 
 /* eslint-disable-next-line  @typescript-eslint/no-empty-interface */
 export interface PortfolioProps {
 }
 
 export const Portfolio: FC<PortfolioProps> = (props) => {
+  // TODO: Retrieve link data from Contentful.
+  const linkData = [
+    { href: "mailto:erkjbro@erikjbrown.tech", text: "Email" },
+    { href: "https://www.linkedin.com/in/erkjbro/", text: "LinkedIn" },
+    { href: "https://github.com/erkjbro", text: "GitHub" },
+    { href: "https://twitter.com/erkjbro", text: "Twitter" },
+    { href: "https://www.upwork.com/fl/erkjbro", text: "Upwork" }
+  ];
+
   return (
     <PortfolioContainer>
       <div>
@@ -15,13 +24,39 @@ export const Portfolio: FC<PortfolioProps> = (props) => {
         <code>Working on an update to pull homepage data from Contentful...</code>
         <Hr />
         <StyledList>
-          <li><a href="mailto:erkjbro@erikjbrown.tech">E-Mail</a></li>
-          <li><a href="https://www.linkedin.com/in/erkjbro/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-          <li><a href="https://github.com/erkjbro" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-          <li><a href="https://twitter.com/erkjbro" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-          <li><a href="https://www.upwork.com/fl/erkjbro" target="_blank" rel="noopener noreferrer">Upwork</a></li>
+          {linkData.map((link, i) => (
+            <HtmlLink href={link.href} key={i}>{link.text}</HtmlLink>
+          ))}
         </StyledList>
       </Main>
     </PortfolioContainer>
   );
+};
+
+interface HtmlLinkProps {
+  children: ReactNode;
+  href: string;
+}
+
+const HtmlLink: FC<HtmlLinkProps> = ({ href, children }) => {
+  const aProps = isValidHttpUrl(href) ? ({
+    href,
+    hrefLang: "en-US",
+    target: "_blank",
+    rel: "noopener noreferrer"
+  }) : ({
+    href,
+    target: "_blank"
+  });
+
+  return (
+    <li><a {...aProps}>
+      {children}
+    </a></li>
+  );
+};
+
+const isValidHttpUrl = (val: string): boolean => {
+  const re = new RegExp("^(http|https)://", "i");
+  return re.test(val);
 };
