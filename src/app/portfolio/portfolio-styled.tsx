6bf80e5fefcf840bb44481a8d4051411f6d22926
Erik J Brown <erkjbro@erikjbrown.tech>
2023-03-06 18:57:55 -0600

    styling links

--- src/app/portfolio/portfolio-styled.tsx
+++ src/app/portfolio/portfolio-styled.tsx
@@ -15,6 +15,36 @@ export const Main = styled.div`
 `;
 
 export const Hr = styled.hr`
-  margin: 2rem 0;
+  margin: 2rem 0 1rem 0;
   color: black;
+`;
+
+export const StyledList = styled.ul`
+  margin: 0.5rem;
+  padding: 0.5rem 0.2rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  height: fit-content;
+  
+  li {
+    display: flex;
+    align-content: center;
+  }
+  
+  li a {
+    display: flex;    
+    padding: 0.2rem 0.4rem;
+    border: 2px solid transparent;
+    align-items: center;
+    color: #11114a;
+    font-size: 1.2rem;
+    font-weight: bold;
+    text-decoration: none;
+    
+    &:hover {
+      border: 2px solid #11114a;
+      border-radius: 10px;
+    }
+  }
 `;
--- src/app/portfolio/portfolio.tsx
+++ src/app/portfolio/portfolio.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Hr, Main, PortfolioContainer } from './portfolio-styled';
+import { Hr, Main, PortfolioContainer, StyledList } from './portfolio-styled';
 
 /* eslint-disable-next-line  @typescript-eslint/no-empty-interface */
 export interface PortfolioProps {
@@ -14,13 +14,13 @@ export const Portfolio: FC<PortfolioProps> = (props) => {
       <Main>
         <code>Working on an update to pull homepage data from Contentful...</code>
         <Hr />
-        <ul style={{ padding: '0', display: 'flex', justifyContent: 'space-between' }}>
+        <StyledList>
           <li><a href="mailto:erkjbro@erikjbrown.tech">E-Mail</a></li>
           <li><a href="https://www.linkedin.com/in/erkjbro/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
           <li><a href="https://github.com/erkjbro" target="_blank" rel="noopener noreferrer">GitHub</a></li>
           <li><a href="https://twitter.com/erkjbro" target="_blank" rel="noopener noreferrer">Twitter</a></li>
           <li><a href="https://www.upwork.com/fl/erkjbro" target="_blank" rel="noopener noreferrer">Upwork</a></li>
-        </ul>
+        </StyledList>
       </Main>
     </PortfolioContainer>
   );
