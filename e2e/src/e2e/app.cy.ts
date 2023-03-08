7ef568db1005ae69f31aea4f5660d9fad404cda2
Erik J Brown <erkjbro@erikjbrown.tech>
2023-03-08 12:11:34 -0600

    Fixing e2e test

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,13 @@
-import { getGreeting } from '../support/app.po';
+import { getHeading } from '../support/app.po';
 
 describe('erkjbro-tech', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display heading on portfolio page', () => {
     // // Custom command example, see `../support/commands.ts` file
     // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Erik J Brown Tech LLC');
+    getHeading().contains('Erik J Brown');
   });
 });
